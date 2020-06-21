0d7cb3298945299b9d26e9c6dc0ae3462b496df9
Filip Timofiejew <ftimofiejew@gmail.com>
2020-06-21 13:45:20 +0200

    18.4 Creation of controller

---
+++ src/main/java/com/project/domain/Task.java
@@ -0,0 +1,13 @@
+package com.project.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Task {
+    private Long id;
+    private String title;
+    private String content;
+
+}
