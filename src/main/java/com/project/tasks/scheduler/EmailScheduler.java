01bcaea998ac3ca1fc97ec0dd1f0638629a0f1cd
Filip Timofiejew <ftimofiejew@gmail.com>
2020-08-12 21:19:22 +0200

    New project version

--- src/main/java/com/project/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/project/tasks/scheduler/EmailScheduler.java
@@ -20,13 +20,13 @@ public class EmailScheduler {
     private AdminConfig adminConfig;
 
 //    @Scheduled(cron = "0 0 10 * * *")
-    @Scheduled(fixedDelay = 10000)
-    public void sendInformationEmail() {
-        long size = taskRepository.count();
-        simpleEmailService.send(new Mail(
-                adminConfig.getAdminMail(),
-                SUBJECT,
-                "Currently in database you got: " + size + " tasks"
-        ));
-    }
+//    @Scheduled(fixedDelay = 10000)
+//    public void sendInformationEmail() {
+//        long size = taskRepository.count();
+//        simpleEmailService.send(new Mail(
+//                adminConfig.getAdminMail(),
+//                SUBJECT,
+//                "Currently in database you got: " + size + " tasks"
+//        ));
+//    }
 }
